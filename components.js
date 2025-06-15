7f8ab719999f33b8868febff410158fdb18a5a4f
Vianney HERVÉ <vianneyherve3@gmail.com>
2025-06-15 09:16:13 +0200

    attempt n°3 with fetch

--- components.js
+++
@@ -1,6 +0,0 @@
-export async function load(component) {
-    const response = await fetch(`/components/${component}.html`);
-    const content = await response.text();
-
-    document.body.innerHTML += content;
-}
